27981d0fde3518e9551eeddcfcb02dac9f979c00
Petri Sirkkala <sirpete@iki.fi>
2015-02-11 16:58:45 +0200

    Cleanup

--- brew/nadjan-brew-all.sh
+++ brew/nadjan-brew-all.sh
@@ -11,21 +11,3 @@ brew install caskroom/cask/brew-cask
 brew cask install \
 	caffeine \
 	dropbox
-
-# Disabled
-#	crashplan \
-
-#~ ❯❯❯ brew list
-#brew-cask			protobuf
-#libevent			reattach-to-user-namespace
-#mobile-shell			tmux
-#openssl				vim
-#panamax				wget
-#pkg-config
-#atomicparsley
-
-# Python
-# http://docs.python-guide.org/en/latest/starting/install/osx/
-# brew install python python3
-# https://github.com/kennethreitz/python-guide/blob/master/docs/dev/virtualenvs.rst
-# pip install virtualenv

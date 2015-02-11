8bc8492213c88a23c49e41b6ba3f5d2f8f725f89
Petri Sirkkala <sirpete@iki.fi>
2015-02-11 16:38:02 +0200

    Nadjan MacBook needs a conf too

---
+++ brew/nadjan-brew-all.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#brew doctor
+
+brew install caskroom/cask/brew-cask
+	git \
+	wget
+#brew install --universal --java homebrew/versions/subversion17
+
+brew cask install \
+	caffeine \
+	dropbox
+
+# Disabled
+#	crashplan \
+
+#~ ❯❯❯ brew list
+#brew-cask			protobuf
+#libevent			reattach-to-user-namespace
+#mobile-shell			tmux
+#openssl				vim
+#panamax				wget
+#pkg-config
+#atomicparsley
+
+# Python
+# http://docs.python-guide.org/en/latest/starting/install/osx/
+# brew install python python3
+# https://github.com/kennethreitz/python-guide/blob/master/docs/dev/virtualenvs.rst
+# pip install virtualenv

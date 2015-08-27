3916819d583af815bcb77da84a11006b337573f3
Petri Sirkkala <sirpete@iki.fi>
2015-08-27 22:47:23 +0300

    Script to pop up presto for me

---
+++ zsh/install-prezto.sh
@@ -0,0 +1,10 @@
+#!/usr/local/bin/zsh
+(
+cd
+ln -sf proj/prezto .zprezto 
+
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+)

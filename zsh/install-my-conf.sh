179f6f1dec2bcb8f8257b8de812f6c927a01ccec
Petri Sirkkala <sirpete@iki.fi>
2015-10-31 20:44:21 +0200

    Install script for my zsh tweaks

---
+++ zsh/install-my-conf.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+(
+    cd
+    localconf=zshrc.$(hostname).local
+    if [ -e proj/dotfiles-sirkkalap/zsh/${localconf} ]; then
+        ln -sf proj/dotfiles-sirkkalap/zsh/${localconf} .${localconf}
+    fi
+)

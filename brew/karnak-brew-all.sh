972d9a23963dd83252b3fea940d62a5e69812ccf
Petri Sirkkala <sirpete@iki.fi>
2015-12-23 14:56:34 +0200

    Update karnak-brew-all.sh

--- brew/karnak-brew-all.sh
+++ brew/karnak-brew-all.sh
@@ -4,77 +4,50 @@
 # curl -sSL https://raw.githubusercontent.com/sirkkalap/dotfiles-sirkkalap/master/brew/karnak-brew-all.sh | bash -
 brew update
 
-brew install caskroom/cask/brew-cask
+brew tap caskroom/cask
 brew install \
   ansible \
   autoconf \
   autojump \
   automake \
-  boot2docker \
   colordiff \
   coreutils \
   diff-pdf \
   docker \
   exiftool \
-  fontconfig \
-  freetype \
   git \
-  gobject-introspection \
-  jpeg \
   leiningen \
   libevent \
-  libpng \
-  libtiff \
-  libtool \
-  libxml2 \
-  libxslt \
-  libyaml \
-  little-cms2 \
-  makedepend \
   maven \
   mercurial \
   mobile-shell \
   mongodb \
-  neon \
   nvm \
-  pixman \
-  pkg-config \
-  poppler \
-  protobuf \
   python \
   python3 \
   readline \
   reattach-to-user-namespace \
-  scons \
-  sqlite \
   ssh-copy-id \
-  subversion17 \
   tcptraceroute \
   tmux \
   tree \
   unrar \
   watch \
   wget \
-  wxmac \
-  xz \
   zsh
 
 #brew install --universal --java homebrew/versions/subversion17
 
 brew cask install \
   bettertouchtool \
-	dropbox \
-	etrecheck \
-	github \
-	google-chrome \
-	hipchat \
-	iterm2 \
-	sizeup \
-	sublime-text \
+  etrecheck \
+  github \
+  google-chrome \
+  hipchat \
+  iterm2 \
+  sublime-text \
   vagrant \
-  vagrant-manager \
-	virtualbox \
-	wireshark
+  wireshark
 
 # Python
 # http://docs.python-guide.org/en/latest/starting/install/osx/

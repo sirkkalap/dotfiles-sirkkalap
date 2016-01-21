39b2de2a57f7cdff7fb2e841719b0a200064a667
Petri Sirkkala <sirpete@iki.fi>
2016-01-21 14:23:21 +0200

    Now install prezto too (to ~/.zprezto)

--- zsh/install-prezto.sh
+++ zsh/install-prezto.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env zsh
-(
-cd
-ln -sf proj/prezto .zprezto 
+if [ ! -e ${ZDOTDIR:-$HOME}.zprezto ]; then
+  git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+fi
 
-setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-done
+(
+  setopt EXTENDED_GLOB
+  for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+    ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+  done
 )

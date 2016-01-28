c531c0d57bac82638c2d1808c3412dda8de96beb
Petri Sirkkala <sirpete@iki.fi>
2016-01-28 22:41:35 +0200

    Fixed relative calls. Added Bonaqua

--- zsh/install-my-conf.sh
+++ zsh/install-my-conf.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
+BASEDIR=$(cd "$(dirname "$0")"; pwd)
+
 (
     cd
-    localconf=zshrc.$(hostname).local
-    if [ -e proj/dotfiles-sirkkalap/zsh/${localconf} ]; then
-        ln -sf proj/dotfiles-sirkkalap/zsh/${localconf} .${localconf}
+    localconf="zshrc.$(hostname)"
+    if [ -e "$BASEDIR/${localconf}" ]; then
+        ln -sf "$BASEDIR/${localconf}" .${localconf}
+    fi
+    if [ ! -e ~/bin/functions_sirkkalap.sh ]; then
+        mkdir -p ~/bin
+        ln -sf "$BASEDIR/../functions_sirkkalap.sh" ~/bin/functions_sirkkalap.sh
     fi
 )

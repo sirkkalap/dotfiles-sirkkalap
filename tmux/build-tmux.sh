86ca02b0d7f9e4e095388d1ba7521e07e7c35f57
Petri Sirkkala <sirpete@iki.fi>
2016-02-24 13:05:25 +0200

    Update build-tmux.sh

--- tmux/build-tmux.sh
+++ tmux/build-tmux.sh
@@ -2,13 +2,22 @@
 # pull and compile tmux
 # https://github.com/tmux/tmux/blob/master/README
 
-# Dependencies: automake, libevent, libevent-devel
+# Dependencies: automake
 
 set -e # Fail fast
 
-git clone https://github.com/tmux/tmux.git
-cd tmux
-sh autogen.sh
-./configure && make
+(
+  curl -sSL https://github.com/libevent/libevent/releases/download/release-2.0.22-stable/libevent-2.0.22-stable.tar.gz
+  tar zxf libevent-2.0.22-stable.tar.gz
+  cd libevent-2.0.22
+  ./configure && make
+)
 
-echo "run \nsudo install\n here, if you want to install globally"
+(
+  git clone https://github.com/tmux/tmux.git
+  cd tmux
+  sh autogen.sh
+  ./configure && make
+)
+
+echo "run \nsudo tmux/install\n, if you want to install globally"

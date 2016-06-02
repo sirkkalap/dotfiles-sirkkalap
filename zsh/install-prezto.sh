0eedece0e5cc433a4cd7f713e2d9b43e424b2a22
Petri Sirkkala <sirpete@iki.fi>
2016-06-02 21:39:50 +0300

    Force links
    So we do not fail when upgrading

--- zsh/install-prezto.sh
+++ zsh/install-prezto.sh
@@ -6,6 +6,6 @@ fi
 (
   setopt EXTENDED_GLOB
   for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-    ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+    ln -sf "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
   done
 )

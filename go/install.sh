0d70f32470b7e1872605e64d37c9954514101918
Petri Sirkkala <sirpete@iki.fi>
2019-03-29 14:27:17 +0200

    Version and checksum variables

--- go/install.sh
+++ go/install.sh
@@ -9,9 +9,11 @@ if [[ ${distro} == darvin ]]; then
 fi
 
 if [[ ${DISTRO} == Ubuntu* ]] || [[ ${distro} == centos* ]]; then
-    curl -O https://dl.google.com/go/go1.10.3.linux-amd64.tar.gz
-    sha256sum=$(sha256sum go1.10.3.linux-amd64.tar.gz | cut -f1 -d' ')
-    if [[ ${sha256sum} != "fa1b0e45d3b647c252f51f5e1204aba049cde4af177ef9f2181f43004f901035" ]]; then
+    go_version=go1.12.1
+    go_checksum=2a3fdabf665496a0db5f41ec6af7a9b15a49fbe71a85a50ca38b1f13a103aeec
+    curl -O $https://dl.google.com/go/${go_version}linux-amd64.tar.gz
+    sha256sum=$(sha256sum ${go_version}.linux-amd64.tar.gz | cut -f1 -d' ')
+    if [[ "${sha256sum}" != "${go_checksum}" ]]; then
         echo "Tar file sha256sum mismatch. Corrupted download or tainted source. Exiting."
         exit 127
     fi

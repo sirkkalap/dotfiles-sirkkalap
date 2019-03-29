b27eea3e0f092eb52e6107171102a5ba8145411e
Petri Sirkkala <sirpete@iki.fi>
2019-03-29 12:46:49 +0200

    Distro specific build

--- docker/cred_helper/install.sh
+++ docker/cred_helper/install.sh
@@ -3,8 +3,24 @@
 # https://github.com/docker/docker-credential-helpers
 BASEDIR=$(cd $(dirname $0); /bin/pwd)
 
-source ${BASEDIR}/../../utils_sirkkalap.sh
+source ~/bin/utils_sirkkalap.sh
 
 go get github.com/docker/docker-credential-helpers
-cd $GOPATH/docker/docker-credentials-helpers
-make
+cd docker-credentials-helpers
+
+distro=$(utils_sirkkalap::distro)
+
+if [[ ${distro} == centos* ]]; then
+    service=secretservice
+fi
+
+if [[ ${distro} == Ubuntu* ]]; then
+    service=secretservice
+fi
+
+if [[ ${distro} == darwin ]]; then
+    service=osxkeychain
+fi
+
+make ${service}
+

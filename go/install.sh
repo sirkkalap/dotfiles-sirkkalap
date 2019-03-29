538a54a135f24403d54f3e0d447fc012085e35f8
Petri Sirkkala <sirpete@iki.fi>
2019-03-29 14:27:38 +0200

    Typo

--- go/install.sh
+++ go/install.sh
@@ -11,7 +11,7 @@ fi
 if [[ ${DISTRO} == Ubuntu* ]] || [[ ${distro} == centos* ]]; then
     go_version=go1.12.1
     go_checksum=2a3fdabf665496a0db5f41ec6af7a9b15a49fbe71a85a50ca38b1f13a103aeec
-    curl -O $https://dl.google.com/go/${go_version}linux-amd64.tar.gz
+    curl -O https://dl.google.com/go/${go_version}linux-amd64.tar.gz
     sha256sum=$(sha256sum ${go_version}.linux-amd64.tar.gz | cut -f1 -d' ')
     if [[ "${sha256sum}" != "${go_checksum}" ]]; then
         echo "Tar file sha256sum mismatch. Corrupted download or tainted source. Exiting."

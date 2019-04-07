de4c8818c35e784d5f5a91809740662eaa507058
Petri Sirkkala <sirpete@iki.fi>
2019-04-07 21:48:43 +0300

    Fixed distro var name, fixed file naming, print out go installation directory, not the contents of it

--- go/install.sh
+++ go/install.sh
@@ -8,10 +8,10 @@ if [[ ${distro} == darvin ]]; then
     brew install go
 fi
 
-if [[ ${DISTRO} == Ubuntu* ]] || [[ ${distro} == centos* ]]; then
+if [[ ${distro} == Ubuntu* ]] || [[ ${distro} == centos* ]]; then
     go_version=go1.12.1
     go_checksum=2a3fdabf665496a0db5f41ec6af7a9b15a49fbe71a85a50ca38b1f13a103aeec
-    curl -O https://dl.google.com/go/${go_version}linux-amd64.tar.gz
+    curl -O https://dl.google.com/go/${go_version}.linux-amd64.tar.gz
     sha256sum=$(sha256sum ${go_version}.linux-amd64.tar.gz | cut -f1 -d' ')
     if [[ "${sha256sum}" != "${go_checksum}" ]]; then
         echo "Tar file sha256sum mismatch. Corrupted download or tainted source. Exiting."
@@ -19,15 +19,15 @@ if [[ ${DISTRO} == Ubuntu* ]] || [[ ${distro} == centos* ]]; then
     fi
 
     rm -rf ./go
-    tar xf go1.10.3.linux-amd64.tar.gz
-    rm -f go1.10.3.linux-amd64.tar.gz
+    tar xf ${go_version}.linux-amd64.tar.gz
+    rm -f ${go_version}.linux-amd64.tar.gz
 
     root_group=$(id -g -n root)
+
+    utils_sirkkalap::block "Installing to /usr/local/go"
     sudo chown -R root:${root_group} ./go
     sudo rm -rf /usr/local/go
     sudo mv go /usr/local
-
-    echo "Installed go to /usr/local/go/*"
 fi
 
 # Default project folder and default install folder

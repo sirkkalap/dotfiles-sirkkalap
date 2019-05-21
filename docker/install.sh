cdfd7a888310bdd547cc839ff7337635c7e5b8ad
Petri Sirkkala <sirpete@iki.fi>
2019-05-21 14:01:28 +0300

    Improved and better something

--- docker/install.sh
+++ docker/install.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 
-sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
-curl -fssl https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-sudo apt update
-sudo apt install docker-ce
-sudo usermod -aG docker ${USER}
+source ~/bin/utils_sirkkalap.sh
+
+distro=$(utils_sirkkalap::distro)
+
+if [[ "${distro}" == Ubuntu* ]]; then
+    sudo apt update
+    sudo apt install apt-transport-https ca-certificates curl software-properties-common
+    curl -fssl https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+    sudo apt update
+    sudo apt install docker-ce
+    sudo usermod -aG docker ${USER}
+fi
+
+if [[ "${distro}" == darvin ]]; then
+    brew cask install docker
+    open /Applications/Docker.app
+fi

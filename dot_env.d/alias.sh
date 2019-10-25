552f2e5740cf67bd06b03f1ce8baec7a05374a1e
Petri Sirkkala <sirpete@iki.fi>
2019-10-25 17:14:14 +0300

    Stop prezto babysitting me with file operations

--- dot_env.d/alias.sh
+++ dot_env.d/alias.sh
@@ -1,2 +1,12 @@
 alias curlh="curl -i -v -s -o /dev/null 2>&1"
 alias dnetwork="docker network ls -q | xargs docker network inspect --format='{{.Name}} {{range .IPAM.Config}}{{.Subnet}}{{end}}'"
+
+# Remove prezto babysitting me
+alias cp='nocorrect cp'
+alias ln='nocorrect ln'
+alias mv='nocorrect mv'
+alias rm='nocorrect rm'
+alias cpi="${aliases[cp]:-cp} -i"
+alias lni="${aliases[ln]:-ln} -i"
+alias mvi="${aliases[mv]:-mv} -i"
+alias rmi="${aliases[rm]:-rm} -i"

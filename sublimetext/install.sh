ee615ac7e05802c8cdcf312fbb4c240cf5a7c551
Petri Sirkkala <sirpete@iki.fi>
2020-02-10 14:25:44 +0200

    Ubuntu install, conf-sync

--- sublimetext/install.sh
+++ sublimetext/install.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
-
+# https://linuxize.com/post/how-to-install-sublime-text-3-on-ubuntu-18-04/
 source ~/bin/utils_sirkkalap.sh
 
 distro=$(utils_sirkkalap::distro)
 
 if [[ "${distro}" == Ubuntu* ]]; then
-    echo "Sorry, I do not know how to install java on Ubuntu yet..."
-    exit 1
+    sudo apt update
+    sudo apt install apt-transport-https ca-certificates curl software-properties-common
+    curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+    sudo add-apt-repository "deb https://download.sublimetext.com/ apt/stable/"
+    sudo apt update
+    sudo apt install sublime-text
 fi
 
 if [[ "${distro}" == darwin ]]; then

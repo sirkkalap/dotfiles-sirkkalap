0832b703e4d535a1e41e5e4b43eb60b653a2bdb2
Petri Sirkkala <sirpete@iki.fi>
2020-02-11 14:03:08 +0200

    Sublime Text synkkaus

---
+++ sublimetext/conf-sync.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+source ~/bin/utils_sirkkalap.sh
+
+distro=$(utils_sirkkalap::distro)
+
+function clone_sync_repo_if_not_exists()
+{
+    local CONF_DIR=$1
+    if [[ ! -d ${CONF_DIR}/.git ]]; then
+        rm -rf ${CONF_DIR}
+        git clone git@github.com:sirkkalap/sublime-text-settings-sirkkalap.git ${CONF_DIR}
+    else
+        echo "Found ${CONF_DIR}/.git folder. Not touching the configuration folder."
+    fi
+}
+
+if [[ "${distro}" == Ubuntu* ]]; then
+	clone_sync_repo_if_not_exists ${HOME}/.config/sublime-text-3/Packages/User
+fi
+
+if [[ "${distro}" == darwin ]]; then
+	clone_sync_repo_if_not_exists ${HOME}/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+fi

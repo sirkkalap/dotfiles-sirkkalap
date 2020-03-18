616918c3fc6475647d43b9b708568452fd316a1b
Petri Sirkkala <petri.sirkkala@solita.fi>
2020-03-18 22:17:17 +0200

    VBox DHCP CIDR -> 10.30.30.16...

---
+++ VirtualBox/vboxdhcp.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+VBoxManage dhcpserver modify --network=HostInterfaceNetworking-vboxnet0 --server-ip=10.30.30.17 --lower-ip=10.30.30.18 --upper-ip=10.30.30.30 --netmask=255.255.255.240
+VBoxManage dhcpserver modify --network=HostInterfaceNetworking-vboxnet1 --server-ip=10.30.30.33 --lower-ip=10.30.30.34 --upper-ip=10.30.30.46 --netmask=255.255.255.240
+VBoxManage dhcpserver modify --network=HostInterfaceNetworking-vboxnet2 --server-ip=10.30.30.49 --lower-ip=10.30.30.50 --upper-ip=10.30.30.62 --netmask=255.255.255.240
+VBoxManage dhcpserver modify --network=HostInterfaceNetworking-vboxnet3 --server-ip=10.30.30.65 --lower-ip=10.30.30.66 --upper-ip=10.30.30.78 --netmask=255.255.255.240

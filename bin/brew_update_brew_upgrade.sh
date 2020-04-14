0a6d99b485ff94232d3c2937c7c74d2dcb3d7bc2
Petri Sirkkala <petri.sirkkala@solita.fi>
2020-04-14 13:40:25 +0300

    Beautify

--- bin/brew_update_brew_upgrade.sh
+++ bin/brew_update_brew_upgrade.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 BASEDIR=$(cd $(dirname $0); /bin/pwd)
-($BASEDIR/isodate && /usr/local/bin/brew update && /usr/local/bin/brew upgrade --all) >>$HOME/brew-update.log 2>&1
+(
+    $BASEDIR/isodate && \
+    /usr/local/bin/brew update && \
+    /usr/local/bin/brew upgrade
+) >>$HOME/brew-update.log 2>&1

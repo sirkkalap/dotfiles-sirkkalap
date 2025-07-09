a8466da023d9ccc767fbe7203a72fbf51e418023
Petri Sirkkala <sirpete@iki.fi>
2025-07-10 00:40:50 +0300

    exit with non-zero on java_home failure

--- bin/test-java_home.sh
+++ bin/test-java_home.sh
@@ -5,4 +5,7 @@ j18home=$(/usr/libexec/java_home -v 1.8)
 if [ "${j18home}" != "/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home" ]
 then
     echo "java_home execution failed with result: ${j18home}"
+    exit 1
 fi
+
+exit 0

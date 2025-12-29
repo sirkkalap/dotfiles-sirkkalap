dae08ce4e5774c949ac950ef5af0144f55f3aced
Petri Sirkkala <petri.sirkkala@solita.fi>
2025-12-30 00:37:01 +0200

    Remove legacy karnak cask list

---
+++ blender/install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+source ~/bin/install_utils.sh
+install_utils::init "$0"
+
+
+
+PACKAGE=blender
+CASK=
+
+if [[ "${distro}" == Ubuntu* ]]; then
+    echo "Sorry, I do not know how to install $PACKAGE on Ubuntu yet..."
+    exit 1
+fi
+
+if [[ "${distro}" == darwin ]]; then
+    brew $CASK install $PACKAGE </dev/null
+fi
+
